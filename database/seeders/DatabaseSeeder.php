af39bc6e05a15dae104db58c18796e76324269d3
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-02-16 15:29:15 +0100

    complete database things P1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Categorie::factory(10)->create();
+        \App\Models\Post::factory(40)->create();
+        \App\Models\Comment::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
